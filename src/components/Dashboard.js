b99dfb8bf5957cb8d1041bec925ccbd02332462f
Javier Yace Huaman <44007462+yaceh@users.noreply.github.com>
2020-09-11 17:56:11 -0500

    agencias

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -171,43 +171,23 @@ export class Dashboard extends Component {
                     </div>
                 </div>
 
-                <div className="p-col-12 p-md-6 p-xl-3">
+                <div className="p-col-12 p-md-6 p-xl-6">
                     <div className="highlight-box">
-                        <div className="initials" style={{backgroundColor:'#007be5',color:'#00448f'}}><span>TV</span></div>
+                        <div className="initials" style={{backgroundColor:'#007be5',color:'#00448f'}}><span>Agencias</span></div>
                         <div className="highlight-details ">
                             <i className="pi pi-search"/>
-                            <span>Total Queries</span>
-                            <span className="count">523</span>
+                            <span>Total de Agencias</span>
+                            <span className="count">25</span>
                         </div>
                     </div>
                 </div>
-                <div className="p-col-12 p-md-6 p-xl-3">
+                <div className="p-col-4 p-md-6 p-xl-6">
                     <div className="highlight-box">
-                        <div className="initials" style={{backgroundColor:'#ef6262',color:'#a83d3b'}}><span>TI</span></div>
+                        <div className="initials" style={{backgroundColor:'#ef6262',color:'#a83d3b'}}><span>Categorias</span></div>
                         <div className="highlight-details ">
                             <i className="pi pi-question-circle"/>
-                            <span>Total Issues</span>
-                            <span className="count">81</span>
-                        </div>
-                    </div>
-                </div>
-                <div className="p-col-12 p-md-6 p-xl-3">
-                    <div className="highlight-box">
-                        <div className="initials" style={{backgroundColor:'#20d077',color:'#038d4a'}}><span>OI</span></div>
-                        <div className="highlight-details ">
-                            <i className="pi pi-filter"/>
-                            <span>Open Issues</span>
-                            <span className="count">21</span>
-                        </div>
-                    </div>
-                </div>
-                <div className="p-col-12 p-md-6 p-xl-3">
-                    <div className="highlight-box">
-                        <div className="initials" style={{backgroundColor:'#f9c851',color:'#b58c2b'}}><span>CI</span></div>
-                        <div className="highlight-details ">
-                            <i className="pi pi-check"/>
-                            <span>Closed Issues</span>
-                            <span className="count">60</span>
+                            <span>numero de categorias</span>
+                            <span className="count">5</span>
                         </div>
                     </div>
                 </div>
