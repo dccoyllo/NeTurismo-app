005252b360aacc51278b99f80771d38dc96cce5e
david ccoyllo <44007590+dccoyllo@users.noreply.github.com>
2020-09-11 18:34:42 -0500

    modulos home, login

---
+++ src/components/inicioPage.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+export class inicioPage extends Component {
+
+    render() {
+        return (
+            <div className="p-grid">
+                <div className="p-col-12">
+                    <div className="card">
+                        <h1>Está es la página de Inicio</h1>
+                        <p>Use this page to start from scratch and place your custom content.</p>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
